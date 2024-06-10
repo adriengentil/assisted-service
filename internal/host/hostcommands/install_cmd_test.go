6f2aed67f4ef2feb32a8dc9de96d5537fa7f45db
Adrien Gentil <agentil@redhat.com>
2024-06-10 14:49:34 +0200

    import ginkgo table

--- internal/host/hostcommands/install_cmd_test.go
+++ internal/host/hostcommands/install_cmd_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"github.com/openshift/assisted-service/internal/common"
 	eventgen "github.com/openshift/assisted-service/internal/common/events"
